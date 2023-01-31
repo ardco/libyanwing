3eb59f00187871ab988e98b516715dcff44e97b2
ardco <112344657+ardco@users.noreply.github.com>
2023-01-31 12:15:00 +0200

    Add files via upload

---
+++ libyanwing/libyanwing/doctype/business_line/business_line.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, ARD and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('business line', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ libyanwing/libyanwing/doctype/office_location/office_location.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, ARD and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Office Location', {
+	// refresh: function(frm) {
+
+	// }
+});
